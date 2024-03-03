aa8061833a30d57d36fac50c22398ce9f9deb842
Maximsiomin <80629574+Maximsiomin@users.noreply.github.com>
2024-03-03 23:25:48 +0300

    Gradle fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("com.google.gms:google-services:4.4.1")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+        classpath(libs.gradleTools)
+        classpath(libs.googleServicesClasspath)
+        classpath(libs.firebaseCrashlyticsGradleClasspath)
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.dagger.hilt.android") version "2.46.1" apply false
+    alias(libs.plugins.androidApplication) apply(false)
+    alias(libs.plugins.kotlinAndroid) apply(false)
+    alias(libs.plugins.daggerHiltAndroid) apply(false)
 }
